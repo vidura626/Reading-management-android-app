8d6e5178e893b6dc419888357f76c9b14409e572
vidura626 <wijesinghemap@gmail.com>
2023-08-15 21:35:37 +0530

    Home UI and AllBooksActivity created

--- app/src/main/java/com/example/readingmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/readingmanagementsystem/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.readingmanagementsystem;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks;
+    private Button btnAlreadyRead;
+    private Button btnWantedToRead;
+    private Button btnFavorite;
+    private Button btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWantedToRead = findViewById(R.id.btnWantToRead);
+        btnFavorite = findViewById(R.id.btnFavorite);
+        btnAbout = findViewById(R.id.btnAbout);
+    }
+
 }
